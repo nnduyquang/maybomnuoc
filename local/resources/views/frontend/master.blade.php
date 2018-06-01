2a6034187db2c7420819d5239cdc77184f582a5e
loiPH <loiph.vncoder@users.noreply.github.com>
2018-06-01 08:59:02 +0700

    ok

--- local/resources/views/frontend/master.blade.php
+++ local/resources/views/frontend/master.blade.php
@@ -15,53 +15,10 @@
     @yield('styles')
 </head>
 <body>
-<header id="header">
 
-</header>
-
-<div id="blurrMe">
-    @include('frontend.common.menu.m-menu')
-    @include('frontend.common.menu.menu')
-    @yield('slider')
-    @yield('container')
-</div>
-@include('frontend.common.menu.m-sidebar')
-<div class="footer">
-    @include('frontend.common.footer')
-</div>
-{{ Html::script('js/core.common.js') }}
-{{ Html::script('js/core.frontend.js') }}
-<script type="text/javascript">
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-</script>
-{{--@yield('scripts')--}}
-{{ Html::script('js/scripts.js') }}
-<div class="callback d-lg-none d-md-none">
-    <div class="phone_animation">
-        <div class="phone_animation_circle"></div>
-        <div class="phone_animation_circle_fill"></div>
-        <a href="tel:0962599482" class="phone_animation_circle_fill_img"><i class="fas fa-phone"
-                                                                            aria-hidden="true"></i></a>
-    </div>
-</div>
-<div class="callback d-none d-md-block" style="right: 150px;bottom: -30px;left:inherit">
-    <div class="phone_animation">
-        <div class="phone_animation_circle"></div>
-        <div class="phone_animation_circle_fill"></div>
-        <a href="tel:0962599482" class="phone_animation_circle_fill_img"><i class="fas fa-phone"
-                                                                            aria-hidden="true"></i></a>
-    </div>
-</div>
-<div class="mess_desk_bot d-none d-md-block" style="position: fixed;bottom:40px;right: 0px;">
-    <a href="tel:0962599482" style="display: block;width: 260px;height: 56px;background: url({{URL::to('images/nenhot.png')}}) no-repeat;text-align: center;padding-top: 10px;color:#fff;font-size: 20px;font-family: 'roboto-bold'">
-    </a>
-</div>
 
 
 </body>
-
+{{ Html::script('js/core.common.js') }}
+{{ Html::script('js/core.frontend.js') }}
 </Html>
